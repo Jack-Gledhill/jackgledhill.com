e2c793082f49cc0346faae63f21910e289f50a2f
Jack Gledhill <me@jackgledhill.com>
2025-04-02 15:05:49 +0100

    Make some small adjustments to the footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -3,7 +3,7 @@
     import { faGithub } from '@fortawesome/free-brands-svg-icons';
 </script>
 
-<div class="bg-slate-950 p-8 grid grid-rows-2 md:grid-rows-none md:grid-cols-2 gap-8 md:place-content-between">
+<div class="bg-slate-950 p-8 grid grid-rows-2 md:grid-rows-none md:grid-cols-2 gap-8 md:place-content-between text-sm">
     <p class="text-center md:text-left">
         Copyright &copy; 2025
         <a class="underline hover:text-slate-400" href="https://jackgledhill.com">
@@ -11,9 +11,10 @@
         </a>
     </p>
 
-    <p class="text-center md:text-right">
-        <a class="underline hover:text-slate-400" href="https://github.com/Jack-Gledhill/jackgledhill.com">
-            <FontAwesomeIcon icon={faGithub} /> Source code
+    <p class="text-center md:text-right hover:text-slate-400">
+        <FontAwesomeIcon icon={faGithub} />
+        <a class="underline" href="https://github.com/Jack-Gledhill/jackgledhill.com">
+            Source code
         </a>
     </p>
 </div>
