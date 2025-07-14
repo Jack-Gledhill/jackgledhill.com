1f87017622cc99a92571be4c5e3ddb8045936338
Jack Gledhill <me@jackgledhill.com>
2025-07-14 16:14:38 +0100

    Finish off the noddsoc.co.uk project page

---
+++ src/routes/about/noddsoc.co.uk/+page.svelte
@@ -0,0 +1,52 @@
+<script>
+</script>
+
+<h1>About the Project</h1>
+<p>
+    Sheffield's <a href="/about/noddsoc" target="_blank">NoDDSoc</a> is a student society dedicated to TTRPGs.
+    We run various events throughout the year, including one-shots, and larger campaigns.
+    Needless to say, there's a lot of information we need to keep track of and share with our members.
+</p>
+<p>
+    In years past, NoDDSoc had a website that it would use to do all of this, but that website has since been lost to the ether.
+    After I was elected Webmaster, I decided I wanted to rebuild that website from the ground up.
+    Thus, this project was born.
+</p>
+
+<h2>Project Requirements</h2>
+<p>
+    There were a few key considerations I had to keep in mind when developing this project:
+</p>
+<ul>
+    <li>The majority of Committee are not technical, so updating this will be the Webmaster's job</li>
+    <li>Future Webmasters aren't guaranteed to be CompSci students, so the project should be as simple as possible to make changes to</li>
+    <li>The website needed to be accessible on all types of devices and to all types of users</li>
+    <li>Ideally, the project should be able to host an online library that would let people borrow books from us</li>
+    <li>If possible, the project should be able to host an online newsletter that we can use to update our members on what's going on</li>
+</ul>
+
+<h2>Technologies Used</h2>
+<p>
+    There were a few technologies I considered in planning for this project, the big ones being:
+</p>
+<ul>
+    <li><a href="https://svelte.dev" target="_blank">Svelte</a> (which is what this website is written in)</li>
+    <li>Raw HTML & CSS files</li>
+    <li>A website builder like <a href="https://sites.google.com" target="_blank">Google Sites</a></li>
+    <li><a href="https://www.mkdocs.org" target="_blank">MkDocs</a> and its various plugins & themes</li>
+</ul>
+<p>
+    In the end, I settled on <a href="https://squidfunk.github.io/mkdocs-material/" target="_blank">Material for MkDocs</a>, a beautiful theme for MkDocs that also provides a lot of additional functionality that would be incredibly useful for this project.
+    The main reasons I chose Material for this project include:
+</p>
+<ul>
+    <li>The site's content is primarily Markdown, so should be simple for a future Webmaster to learn</li>
+    <li>The theme includes both a blogging system and tagging, which would allow us to have a newsletter and online library respectively</li>
+    <li>The interface is intuitive, accessible and easy to work with</li>
+</ul>
+
+<h2>The Project Going Forward</h2>
+<p>
+    Now that we're comfortably settled in with Material, there's not much else left to do besides write the content.
+    It's still a work in progress, and there's lots more to be done, but the foundation has been laid and the technical aspects are finished.
+</p>
