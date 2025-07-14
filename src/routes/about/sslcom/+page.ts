65927b50ad910f2886968993e29e4a109e788e04
Jack Gledhill <me@jackgledhill.com>
2025-07-14 14:12:40 +0100

    Add empty page for SSLCom

---
+++ src/routes/about/sslcom/+page.ts
@@ -0,0 +1,13 @@
+import type { LayoutLoad } from './$types';
+
+export const load: LayoutLoad = () => {
+	return {
+		name: "Staff-Student Liaison Committee",
+		start: "Sep 2024",
+		end: "present",
+		position: "Academic Representative",
+		banner: "/images/sslcom-banner.jpg",
+		logo: "/images/ssu.png",
+		links: {}
+	};
+};
