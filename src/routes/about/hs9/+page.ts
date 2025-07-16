e93065d94c9f0753e4c8e05cc8ca8923e0fe4a56
Jack Gledhill <me@jackgledhill.com>
2025-07-16 12:34:39 +0100

    Add SEO the about pages, improve default SEO and replace twittercard

--- src/routes/about/hs9/+page.ts
+++ src/routes/about/hs9/+page.ts
@@ -2,6 +2,17 @@ import type { LayoutLoad } from './$types';
 
 export const load: LayoutLoad = () => {
 	return {
+        meta: {
+            title: "HackSheffield 9 ~ Jack Gledhill",
+            description: "Sheffield's ninth hackathon, and the third largest hackathon in the UK. HackSheffield was organised by Sheffield CompSoc and took place in November 2024. The event brought together students from across the UK to collaborate on innovative projects over a weekend.",
+            keywords: [
+                "Hackathon",
+                "HackSheffield",
+                "HackSheffield 9",
+                "Sheffield hackathon",
+                "Sheffield CompSoc"
+            ]
+        },
 		name: "HackSheffield 9",
 		start: "November 2024",
 		banner: "/images/hs9-banner.jpg",
--- src/routes/about/k8s/+page.ts
+++ src/routes/about/k8s/+page.ts
@@ -2,6 +2,21 @@ import type { LayoutLoad } from './$types';
 
 export const load: LayoutLoad = () => {
 	return {
+        meta: {
+            title: "Kubernetes Workshop ~ Jack Gledhill",
+            description: "This was an introductory workshop on Kubernetes, planned and ran by Jack Gledhill. The workshop introduces Kubernetes, its uses and how to get started with it. Students learn the basics of container orchestration, setting up a cluster with Docker Desktopm and deploying Pods and Services to the cluster.",
+            keywords: [
+                "Kubernetes",
+                "University of Sheffield",
+                "Sheffield Students' Union",
+                "Sheffield CompSoc",
+                "Container orchestration",
+                "Docker",
+                "Kubernetes Engineer",
+                "DevOps",
+                "DevOps Engineer"
+            ]
+        },
 		name: "Kubernetes Workshop",
 		start: "April 2025",
 		banner: "/images/k8s-banner.jpg",
