62c3250b800e4dc2d01547f812d9676a8c081549
Jack Gledhill <me@jackgledhill.com>
2026-03-12 17:00:51 +0000

    Move hero to its own component and replace some static content with md frontmatter

--- src/content/index.md
+++ src/content/index.md
@@ -20,16 +20,20 @@ keywords:
   - React
   - Svelte
   - Docker
-email: me@jackgledhill.com
-links:
-  github: Jack-Gledhill
-  linkedin: jackgledhill
-  orcid: 0009-0000-3055-0714
-positions:
-  - title: Web Developer
-    company: Sheffield Students' Union
-    href: https://su.sheffield.ac.uk
-  - title: MEng Software Engineering
-    company: University of Sheffield
-    href: https://sheffield.ac.uk
+hero:
+  title: Jack Gledhill
+  subtitle: Software Engineer
+  image: /images/portrait.webp
+  email: me@jackgledhill.com
+  socials:
+    github: Jack-Gledhill
+    linkedin: jackgledhill
+    orcid: 0009-0000-3055-0714
+  positions:
+    - title: Web Developer
+      company: Sheffield Students' Union
+      href: https://su.sheffield.ac.uk
+    - title: MEng Software Engineering
+      company: University of Sheffield
+      href: https://sheffield.ac.uk
 ---
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,23 +3,9 @@
 	import Committee from '$lib/components/home/committee.svelte';
 	import Event from '$lib/components/home/event.svelte';
 	import Footer from '$lib/components/footer.svelte';
+	import Hero from '$lib/components/home/hero.svelte';
 	import Paper from '$lib/components/home/paper.svelte';
 	import Project from '$lib/components/home/project.svelte';
-
-	import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
-	import {
-		faEarthEurope,
-		faEnvelope,
-		faPrint,
-		faRss
-	} from '@fortawesome/free-solid-svg-icons';
-	import {
-		faDiscord,
-		faGithub,
-		faInstagram,
-		faLinkedin,
-		faOrcid
-	} from '@fortawesome/free-brands-svg-icons';
 
 	let { data } = $props();
 </script>
@@ -27,80 +13,7 @@
 <Announcement />
 
 <div class="p-4 lg:p-16">
-	<div class="w-full bg-slate-800 rounded-lg border-1 border-slate-700 py-4 mb-4 lg:mb-8">
-		<div class="bg-slate-700 grid grid-cols-1 lg:grid-cols-5 2xl:grid-cols-9 gap-y-[1px] lg:gap-y-0 lg:gap-x-[1px]">
-			<div class="col-span-1 p-8 bg-slate-800">
-				<img class="rounded-full shadow-xl md:max-lg:w-1/2 md:max-lg:mx-auto" src="/images/portrait.webp" alt="Jack surrounded by greenery" fetchpriority="high" />
-			</div>
-
-			<div class="col-span-2 2xl:col-span-4 p-8 bg-slate-800">
-				<h1 class="font-bold text-3xl md:text-5xl">Jack Gledhill</h1>
-				<p class="text-xl lg:text-2xl pb-8">Software Engineer</p>
-
-				<div class="text-3xl lg:text-2xl grid grid-cols-3 md:grid-cols-6 gap-8">
-					{#if data.metadata.links.discord}
-						<a class="hover:text-slate-400" href={`https://discord.gg/${data.metadata.links.discord}`} target="_blank" aria-label="Discord">
-							<FontAwesomeIcon icon={faDiscord} fixedWidth />
-						</a>
-					{/if}
-
-					{#if data.metadata.links.github}
-						<a class="hover:text-slate-400" href={`https://github.com/${data.metadata.links.github}`} target="_blank" aria-label="GitHub">
-							<FontAwesomeIcon icon={faGithub} fixedWidth />
-						</a>
-					{/if}
-
-					{#if data.metadata.links.instagram}
-						<a class="hover:text-slate-400" href={`https://www.instagram.com/${data.metadata.links.instagram}`} target="_blank" aria-label="Instagram">
-							<FontAwesomeIcon icon={faInstagram} fixedWidth />
-						</a>
-					{/if}
-
-					{#if data.metadata.links.linkedin}
-						<a class="hover:text-slate-400" href={`https://www.linkedin.com/company/${data.metadata.links.linkedin}`} target="_blank" aria-label="LinkedIn">
-							<FontAwesomeIcon icon={faLinkedin} fixedWidth />
-						</a>
-					{/if}
-
-					{#if data.metadata.links.orcid}
-						<a class="hover:text-slate-400" href={`https://orcid.org/${data.metadata.links.orcid}`} target="_blank" aria-label="OrcID">
-							<FontAwesomeIcon icon={faOrcid} fixedWidth />
-						</a>
-					{/if}
-
-					{#if data.metadata.links.website}
-						<a class="hover:text-slate-400" href={`${data.metadata.links.website}`} target="_blank" aria-label="Website">
-							<FontAwesomeIcon icon={faEarthEurope} fixedWidth />
-						</a>
-					{/if}
-				</div>
-			</div>
-
-			<div class="col-span-2 2xl:col-span-4 p-8 leading-8 text-sm bg-slate-800">
-				<h2 class="uppercase font-bold text-2xl text-slate-400">Positions</h2>
-				<ul class="list-disc list-outside pl-4">
-					{#each data.metadata.positions as pos (pos.title)}
-						<li>
-							{pos.title} @
-							<a class="underline hover:text-slate-400" href={pos.href}>{pos.company}</a>
-						</li>
-					{/each}
-				</ul>
-
-				<div class="pt-4 grid md:grid-cols-3 gap-8 text-center text-sm">
-					<a class="p-4 rounded-lg bg-blue-500 hover:bg-blue-700 transition duration-200 shadow-md" href="/cv.pdf" target="_blank">
-						<FontAwesomeIcon icon={faPrint} fixedWidth /> Download my CV
-					</a>
-					<a class="p-4 rounded-lg bg-blue-500 hover:bg-blue-700 transition duration-200 shadow-md" href={`mailto:${data.metadata.email}`} target="_blank">
-						<FontAwesomeIcon icon={faEnvelope} fixedWidth /> Send me an email
-					</a>
-					<a class="p-4 rounded-lg bg-blue-500 hover:bg-blue-700 transition duration-200 shadow-md" href="https://blog.jackgledhill.com" target="_blank">
-						<FontAwesomeIcon icon={faRss} fixedWidth /> Read my blog
-					</a>
-				</div>
-			</div>
-		</div>
-	</div>
+	<Hero metadata={data.metadata.hero} />
 
 	<div class="w-full grid grid-cols-1 lg:grid-cols-3 gap-y-4 lg:gap-8">
 		<div class="col-span-1 bg-slate-800 rounded-lg border-1 border-slate-700 p-8 md:max-lg:grid md:max-lg:grid-cols-2 md:max-lg:gap-4">
