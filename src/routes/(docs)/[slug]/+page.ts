8a44128d56ee5737cee31200d29828877d118864
Jack Gledhill <me@jackgledhill.com>
2026-03-14 20:01:51 +0000

    Generalise copyright into document pages

--- src/routes/(docs)/[slug]/+page.ts
+++ src/routes/(docs)/[slug]/+page.ts
@@ -1,14 +1,14 @@
 import { error } from '@sveltejs/kit';
 
-export async function load() {
+export async function load({ params }) {
     try {
-        const page = await import('/src/content/copyright.md');
+        const page = await import(`../../../content/docs/${params.slug}.md`);
         return {
             metadata: page.metadata,
             default: page.default
         };
     } catch (e) {
         console.log(e);
-        error(404, 'Missing copyright.md content file');
+        error(404, 'Missing content file');
     }
 }
