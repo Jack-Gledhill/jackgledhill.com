0d9740e902e6558946c19e3027482015faaa15fc
Jack Gledhill <me@jackgledhill.com>
2026-03-14 19:57:45 +0000

    Separate main content from blank pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,6 @@
 
     import Announcement from '$lib/components/announcement.svelte';
     import Footer from '$lib/components/footer.svelte';
-    import Hero from '$lib/components/hero.svelte';
-    import Navbar from '$lib/components/navbar.svelte';
 
 	let { children } = $props();
 </script>
@@ -46,14 +44,6 @@
 
 <Announcement />
 
-<div class="p-4 lg:p-16">
-    <Hero metadata={page.data.hero} />
-
-    <Navbar />
-
-    <div class="w-full bg-slate-800 rounded-b-lg lg:rounded-tr-lg border-1 border-slate-700 p-8 flex flex-col gap-8">
-        {@render children()}
-    </div>
-</div>
+{@render children()}
 
 <Footer />
