307b068b8df83023c232bfb2fd987415a57b8454
Jack Gledhill <me@jackgledhill.com>
2026-03-14 19:42:01 +0000

    Move article CSS to app.css so it's not repeated

--- src/routes/(page)/+layout.svelte
+++ src/routes/(page)/+layout.svelte
@@ -6,49 +6,6 @@
     let { children } = $props();
 </script>
 
-<style>
-    @reference '../../app.css';
-
-    :global {
-        article h1,
-        article h2,
-        article h3,
-        article h4,
-        article h5,
-        article h6 {
-            @apply font-bold capitalize;
-        }
-
-        article h1 {
-            @apply text-4xl leading-20 md:leading-48 text-center;
-        }
-
-        article h2 {
-            @apply text-2xl leading-20;
-        }
-
-        article h3 {
-            @apply text-xl leading-16;
-        }
-
-        article > p {
-            @apply text-justify my-8;
-        }
-
-        article a {
-            @apply hover:text-slate-400 underline;
-        }
-
-        article ol {
-            @apply list-decimal list-inside;
-        }
-
-        article ul {
-            @apply list-disc list-inside;
-        }
-    }
-</style>
-
 <div class="p-8 md:p-12 border-b-1 border-slate-600 bg-slate-800">
     <a href="/">
         <FontAwesomeIcon icon={faArrowLeft} fixedWidth />
--- src/routes/events/[slug]/+page.svelte
+++ src/routes/events/[slug]/+page.svelte
@@ -4,49 +4,6 @@
 	let { data } = $props();
 </script>
 
-<style>
-    @reference '../../../app.css';
-
-    :global {
-        article h1,
-        article h2,
-        article h3,
-        article h4,
-        article h5,
-        article h6 {
-            @apply font-bold capitalize;
-        }
-
-        article h1 {
-            @apply text-4xl leading-20 text-center;
-        }
-
-        article h2 {
-            @apply text-2xl leading-20;
-        }
-
-        article h3 {
-            @apply text-xl leading-16;
-        }
-
-        article > p {
-            @apply text-justify my-4;
-        }
-
-        article a {
-            @apply hover:text-slate-400 underline;
-        }
-
-        article ol {
-            @apply list-decimal list-inside;
-        }
-
-        article ul {
-            @apply list-disc list-inside;
-        }
-    }
-</style>
-
 <Back href="/events">
     Back to Events
 </Back>
