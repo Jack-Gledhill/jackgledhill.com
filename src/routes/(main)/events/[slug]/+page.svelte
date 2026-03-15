bd5162a53841d4a89264c24b3924a5be8f60de4d
Jack Gledhill <me@jackgledhill.com>
2026-03-15 13:47:44 +0000

    Remove capitalisation from content page titles

--- src/routes/(main)/events/[slug]/+page.svelte
+++ src/routes/(main)/events/[slug]/+page.svelte
@@ -11,7 +11,7 @@
 </Back>
 
 <section class="text-center flex flex-col gap-2">
-    <h1 class="text-4xl font-bold capitalize">{data.metadata.title}</h1>
+    <h1 class="text-4xl font-bold">{data.metadata.title}</h1>
     <h2 class="text-slate-400 font-bold text-2xl">{data.metadata.type}</h2>
     <p class="italic">{formatDate(data.metadata.date)}</p>
 </section>
