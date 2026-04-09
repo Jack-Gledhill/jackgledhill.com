ac49a123e4c9041d36075b50544dffb00aee09ed
Jack Gledhill <me@jackgledhill.com>
2026-04-09 14:54:39 +0100

    Rename docs to pages

--- src/routes/(pages)/[slug]/+page.ts
+++ src/routes/(pages)/[slug]/+page.ts
@@ -2,7 +2,7 @@ import { error } from '@sveltejs/kit';
 
 export async function load({ params }) {
     try {
-        const page = await import(`../../../content/docs/${params.slug}.md`);
+        const page = await import(`../../../content/pages/${params.slug}.md`);
         return {
             metadata: page.metadata,
             default: page.default
