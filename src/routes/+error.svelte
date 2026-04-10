925676d28d4ff67097ff578e5713360e40c7c324
Jack Gledhill <me@jackgledhill.com>
2026-04-10 10:45:58 +0100

    Add a variable to hide the "report a bug" button on certain errors

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -9,47 +9,56 @@
 		400: { // Bad Request
 			title: "PEBCAK Error",
             message: "You've sent an invalid request. What do you expect me to do with that?",
-            emote: "/images/facepalm.png"
+            emote: "/images/facepalm.png",
+            bugBtn: true
         },
 		401: { // Unauthorized
 			title: "Halt! You got a license for that?",
             message: "You need to authenticate before you can access this page.",
-            emote: "/images/thinking.png"
+            emote: "/images/thinking.png",
+            bugBtn: true
         },
         403: { // Forbidden
 			title: "Missing Permissions",
             message: "Sorry pal, looks like you can't access that content.",
-            emote: "/images/facepalm.png"
+            emote: "/images/facepalm.png",
+            bugBtn: true
         },
 		404: { // Not Found
 			title: "Page Not Found",
             message: "Looks like the page you've requested doesn't exist.",
-            emote: "/images/thinking.png"
+            emote: "/images/thinking.png",
+            bugBtn: true
         },
         418: { // I'm a Teapot
 			title: "Woah! Crazy Easter Egg!",
             message: "I'm a little teapot, short and stout. Here is my handle, here is my spout. When I get all steamed up, hear me shout 'Tip me over and pour me out!'",
-            emote: "/images/tea.png"
+            emote: "/images/tea.png",
+            bugBtn: false
         },
         429: { // Too Many Requests
 			title: "You've Been Ratelimited!",
             message: "Stop, you've violated the ratelimit policy! Pay the Court a fine or serve your sentence!",
             emote: "/images/anger.png",
+            bugBtn: false
         },
         451: { // Unavailable for Legal Reasons
 			title: "For Legal Reasons this Content is Unavailable",
             message: "Move along, move along, nothing to see here. Please allow this button to guide you back home.",
-            emote: "/images/thinking.png"
+            emote: "/images/thinking.png",
+            bugBtn: false
         },
         500: { // Internal Server Error
 			title: "Stop, hammer time!",
             message: "Something's broken. Very broken. An error like this shouldn't happen, please report this as a bug.",
-            emote: "/images/facepalm.png"
+            emote: "/images/facepalm.png",
+            bugBtn: true
         },
         501: { // Not Implemented
 			title: "Under Construction",
             message: "Sorry, looks like that content isn't available yet. Check again later.",
-            emote: "/images/sad.png"
+            emote: "/images/sad.png",
+            bugBtn: false
         }
     };
 </script>
@@ -77,12 +86,14 @@
                     </a>
                 </button>
 
-                <button>
-                    <a class="max-lg:block rounded-lg p-4 bg-red-500 hover:bg-red-700 transition duration-200 shadow-md" href="https://github.com/Jack-Gledhill/jackgledhill.com/issues/new">
-                        <FontAwesomeIcon icon={faGithub} fixedWidth />
-                        Report a bug
-                    </a>
-                </button>
+                {#if (errors[page.status].bugBtn)}
+                    <button>
+                        <a class="max-lg:block rounded-lg p-4 bg-red-500 hover:bg-red-700 transition duration-200 shadow-md" href="https://github.com/Jack-Gledhill/jackgledhill.com/issues/new">
+                            <FontAwesomeIcon icon={faGithub} fixedWidth />
+                            Report a bug
+                        </a>
+                    </button>
+                {/if}
             </div>
         </div>
     </div>
