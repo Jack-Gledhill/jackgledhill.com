88f5bb90bb1dd9dcc811116daa8ebf83e55bea1b
Jack Gledhill <me@jackgledhill.com>
2026-04-13 11:47:21 +0100

    Fix basic page horizontal gutter on tablets

--- src/routes/(pages)/+layout.svelte
+++ src/routes/(pages)/+layout.svelte
@@ -7,8 +7,8 @@
     let { children } = $props();
 </script>
 
-<div class="p-8 md:px-72 md:py-12 flex flex-col gap-4">
-    <a class="text-center lg:text-left" href="/">
+<div class="p-8 md:py-12 lg:px-72 flex flex-col gap-4">
+    <a class="text-center md:text-left" href="/">
         <FontAwesomeIcon icon={faArrowLeft} fixedWidth />
         <span class="underline hover:text-slate-400">Back home</span>
     </a>
