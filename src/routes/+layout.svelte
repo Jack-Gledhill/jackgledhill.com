dfc4ccc09378f4b3b69a20f715ec72cc135a4978
Jack Gledhill <me@jackgledhill.com>
2026-05-22 13:06:12 +0100

    Import fontawesome styles directly and disable library autoload

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import '../app.css';
+    import '@fortawesome/fontawesome-svg-core/styles.css';
+    import { config } from '@fortawesome/fontawesome-svg-core';
     import { Head } from '$lib/components';
+
+    config.autoAddCss = false;
 
     import { page } from '$app/state';
 	let { children } = $props();
