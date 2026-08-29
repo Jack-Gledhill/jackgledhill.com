89abf55af0090e78006a3b4cf922519384f4acef
Jack Gledhill <me@jackgledhill.com>
2026-08-29 10:35:12 +0100

    Correct the error stylesheet applying globally

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,79 +1,90 @@
 <script lang="ts">
-    import "../styles/error.css";
-    import { page } from "$app/state";
+    import { page } from '$app/state';
 
-    import { Button, Head } from "$lib/components";
-    import { Error } from "$lib/content";
-    import { build } from "$lib/site.config.ts";
+    import { Head } from '$lib/components';
+    import { Error } from '$lib/content';
+    import { build } from '$lib/site.config.ts';
 
-    import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-    import { faGithub } from "@fortawesome/free-brands-svg-icons";
-    import { FontAwesomeIcon } from "@fortawesome/svelte-fontawesome";
+    import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+    import { faGithub } from '@fortawesome/free-brands-svg-icons';
+    import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
 
-	const { title, message, emote }: Error.PageProps = Error.getPageProps(page.status);
+    const { title, message, emote }: Error.PageProps = Error.getPageProps(page.status);
 </script>
+
+<style>
+    table th {
+        @apply text-left;
+    }
+
+    table td {
+        @apply text-right;
+    }
+</style>
 
 <Head {title} />
 
 <div class="min-h-screen p-8 lg:px-48 text-center flex flex-col justify-center">
     <div class="grid lg:grid-cols-3 gap-16 items-center">
         <div class="rounded-full mx-auto max-lg:w-3/4 bg-slate-800 border-10 border-slate-600">
-            <enhanced:img src={emote} alt="" />
+            <enhanced:img alt="" class="rounded-b-full" src={emote} />
         </div>
 
         <div class="lg:col-span-2 flex flex-col gap-8">
-            <h1>{page.status}</h1>
-            <h2>{title}</h2>
-            <p>{message}</p>
+            <h1 class="text-8xl font-bold">{page.status}</h1>
+            <h2 class="text-4xl font-bold">{title}</h2>
+            <p class="text-xl">{message}</p>
 
             <div class="flex flex-col lg:flex-row gap-8 justify-center">
-                <Button colour="blue" href="/">
-                    <FontAwesomeIcon icon={faArrowLeft} fixedWidth />
+                <a data-colour="blue" href="/" role="button">
+                    <FontAwesomeIcon fixedWidth icon={faArrowLeft} />
                     Back home
-                </Button>
+                </a>
 
-                <Button colour="red" href={`${build.repository}/issues/new`} target="_blank">
-                    <FontAwesomeIcon icon={faGithub} fixedWidth />
+                <a data-colour="red" href={`${build.repository}/issues/new`} role="button" target="_blank">
+                    <FontAwesomeIcon fixedWidth icon={faGithub} />
                     Report a bug
-                </Button>
+                </a>
             </div>
 
-            <hr />
+            <hr class="border-slate-600 mt-4" />
 
-            <table>
+            <table class="text-slate-400">
                 <tbody>
-                    <tr>
-                        <th>Build time:</th>
-                        <td>{build.buildTime.toLocaleString()}</td>
-                    </tr>
-                    <tr>
-                        <th>Environment:</th>
-                        <td>{build.environment}</td>
-                    </tr>
-                    <tr>
-                        <th>Commit:</th>
-                        <td>
-                            {#if build.commit}
-                                <a href={`${build.repository}/commit/${build.commit}`} target="_blank">{build.commit}</a>
-                            {:else}
-                                N/A
-                            {/if}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>Release:</th>
-                        <td>
-                            {#if build.release}
-                                <a href={`${build.repository}/releases/tag/${build.release}`} target="_blank">{build.release}</a>
-                            {:else}
-                                N/A
-                            {/if}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>SvelteKit Version:</th>
-                        <td>{build.svelteKit}</td>
-                    </tr>
+                <tr>
+                    <th>Build time:</th>
+                    <td>{build.buildTime.toLocaleString()}</td>
+                </tr>
+                <tr>
+                    <th>Environment:</th>
+                    <td>{build.environment}</td>
+                </tr>
+                <tr>
+                    <th>Commit:</th>
+                    <td>
+                        {#if build.commit}
+                            <a data-colour="blue" href={`${build.repository}/commit/${build.commit}`}
+                               target="_blank">{build.commit}</a>
+                        {:else}
+                            N/A
+                        {/if}
+                    </td>
+                </tr>
+                <tr>
+                    <th>Release:</th>
+                    <td>
+                        {#if build.release}
+                            <a data-colour="blue" href={`${build.repository}/releases/tag/${build.release}`}
+                               target="_blank">{build.release}</a>
+                        {:else}
+                            N/A
+                        {/if}
+                    </td>
+                </tr>
+                <tr>
+                    <th>SvelteKit Version:</th>
+                    <td>{build.svelteKit}</td>
+                </tr>
                 </tbody>
             </table>
         </div>
--- src/routes/teapot/+page.svelte
+++ src/routes/teapot/+page.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-    import "../../styles/error.css";
+    import emote from '$lib/assets/emotes/tea.webp?enhanced';
+    import { Head } from '$lib/components';
 
-    import emote from "$lib/assets/emotes/tea.webp?enhanced";
-    import { Button, Head } from "$lib/components";
-
-    import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-    import { FontAwesomeIcon } from "@fortawesome/svelte-fontawesome";
+    import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+    import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
 </script>
 
 <Head title="Woah! Crazy Easter Egg!" />
@@ -13,23 +11,23 @@
 <div class="min-h-screen p-8 lg:px-48 text-center flex flex-col justify-center">
     <div class="grid lg:grid-cols-3 gap-16 items-center">
         <div class="rounded-full mx-auto max-lg:w-3/4 bg-slate-800 border-10 border-slate-600">
-            <enhanced:img src={emote} alt="" />
+            <enhanced:img alt="" class="rounded-b-full" src={emote} />
         </div>
 
         <div class="lg:col-span-2 flex flex-col gap-8">
-            <h1>418</h1>
-            <h2>Woah! Crazy Easter Egg!</h2>
-            <p>
+            <h1 class="text-8xl font-bold">418</h1>
+            <h2 class="text-4xl font-bold">Woah! Crazy Easter Egg!</h2>
+            <p class="text-xl">
                 I'm a little teapot, short and stout.
                 Here is my handle, here is my spout.
                 When I get all steamed up, hear me shout 'Tip me over and pour me out!'
             </p>
 
             <div class="flex flex-col lg:flex-row gap-8 justify-center">
-                <Button colour="blue" href="/">
-                    <FontAwesomeIcon icon={faArrowLeft} fixedWidth />
+                <a data-colour="blue" href="/" role="button">
+                    <FontAwesomeIcon fixedWidth icon={faArrowLeft} />
                     Back home
-                </Button>
+                </a>
             </div>
         </div>
     </div>
